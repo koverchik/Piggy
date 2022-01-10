99829a79e24354decad62e82790c5bef8d525f10
koverchik <koverchik.o@gmail.com>
2022-01-10 15:35:07 +0300

    add: navigate after autorization

--- App/resources/js/components/Authorization/Authorization.tsx
+++ App/resources/js/components/Authorization/Authorization.tsx
@@ -1,10 +1,13 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { GoogleLogin } from 'react-google-login';
+import { useHistory } from 'react-router-dom';
 import store from './../../state';
 import './_Authorization.scss';
 
 const Authorization: React.FC = observer(() => {
+  const history = useHistory();
+  const navigateTo = () => history.push('/');
   return (
     <div className="wrapper-form-user-reg-auth">
       <div className="user-authorization">
@@ -13,7 +16,10 @@ const Authorization: React.FC = observer(() => {
           <GoogleLogin
             clientId="420712854303-99d07k7jiqrsq58c7iv3mugs85oensd2.apps.googleusercontent.com"
             buttonText="Login"
-            onSuccess={(response) => store.UserInfo.getUserInfo(response)}
+            onSuccess={(response) => {
+              store.UserInfo.getUserInfo(response);
+              navigateTo();
+            }}
             onFailure={store.UserInfo.getUserInfo}
             cookiePolicy={'single_host_origin'}
           />
