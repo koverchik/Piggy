3e70ae970290c2162225fe0f20d4fe1356e0675b
koverchik <koverchik.o@gmail.com>
2022-01-22 18:26:00 +0300

    add: pagination on main page

---
+++ App/resources/js/components/PaginationRows/Pagination.tsx
@@ -0,0 +1,58 @@
+import { observer } from 'mobx-react-lite';
+import React from 'react';
+import './_PaginationRows.scss';
+
+type PaginationType = {
+  arrayPaginationNumber: number[];
+  activePart: number;
+  setActivePart: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export const Pagination: React.FC<PaginationType> = observer(
+  ({ arrayPaginationNumber, activePart, setActivePart }) => {
+    console.log(arrayPaginationNumber);
+
+    return (
+      <div className="wrapper-number-pagination">
+        <img
+          src="../images/arrow-left.svg"
+          onClick={() => activePart > 1 && setActivePart((item) => item - 1)}
+          alt="arrow-left"
+          className={
+            activePart == 1
+              ? 'disable-pagination image-pagination'
+              : 'image-pagination'
+          }
+        />
+        {arrayPaginationNumber.map((item: number) => {
+          return (
+            <div
+              key={'pagination' + item}
+              className={
+                activePart == item
+                  ? 'pagination-estimate active-number'
+                  : 'pagination-estimate'
+              }
+              onClick={() => setActivePart(item)}
+            >
+              {item}
+            </div>
+          );
+        })}
+        <img
+          src="../images/arrow-right.svg"
+          onClick={() =>
+            activePart < arrayPaginationNumber.length &&
+            setActivePart((item) => item + 1)
+          }
+          alt="arrow-right"
+          className={
+            activePart == arrayPaginationNumber.length
+              ? 'disable-pagination image-pagination'
+              : 'image-pagination'
+          }
+        />
+      </div>
+    );
+  }
+);
