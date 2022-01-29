446f6bd6a7076b6e6d59e4f19659fda887b36805
koverchik <koverchik.o@gmail.com>
2022-01-29 19:00:10 +0300

    add: package validation form

--- App/resources/js/pages/WalletPage/index.tsx
+++ App/resources/js/pages/WalletPage/index.tsx
@@ -18,7 +18,8 @@ export const OneWallet: React.FC = observer(() => {
   const [activePart, setActivePart] = useState(1);
   const [arrayPaginationNumber, setArrayPaginationNumber] = useState<
     number[]
-  >();
+    >();
+  
   const { id } = useParams<useParamsIdType>();
 
   useEffect(() => {
