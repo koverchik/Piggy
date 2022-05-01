d259f5d2cc1c1b05eb342771b4b7dc7e14f71d10
koverchik <koverchik.o@gmail.com>
2022-05-01 15:01:53 +0300

    add translation for table one wallet

--- App/resources/js/pages/WalletPage/TableOneWallet/index.tsx
+++ App/resources/js/pages/WalletPage/TableOneWallet/index.tsx
@@ -7,6 +7,7 @@ import store from '../../../state';
 import { BodyOneWallet } from './BodyOneWallet';
 import { WalletRowType } from '../types';
 import { Pagination } from '../../../components/Pagination';
+import { useTranslation } from 'react-i18next';
 
 type TableOneWalletType = {
   listRowsWallet: WalletRowType[];
@@ -17,15 +18,16 @@ type TableOneWalletType = {
 
 export const TableOneWallet: React.FC<TableOneWalletType> = observer(
   ({ listRowsWallet, activePart, setActivePart, arrayPaginationNumber }) => {
+    const { t } = useTranslation();
     return (
       <div className="wrapper-tables-list-add">
         <table className="table-list-value">
           <thead>
             <tr>
               <td className="empty-head-item"> </td>
-              <td className="data-head-item">Дата</td>
-              <td className="name-head-one-item">Название</td>
-              <td className="cost-head-one-item"> Стоимость </td>
+              <td className="data-head-item">{t('table.date')}</td>
+              <td className="name-head-one-item">{t('table.name')}</td>
+              <td className="cost-head-one-item">{t('table.cost')} </td>
               <td className="empty-head-item-user"> </td>
             </tr>
           </thead>
@@ -36,9 +38,9 @@ export const TableOneWallet: React.FC<TableOneWalletType> = observer(
           <tfoot>
             <tr>
               <td className="empty-item" colSpan={2}></td>
-              <td className="title-cost-all-item"> Итого: </td>
+              <td className="title-cost-all-item"> {t('table.result')}: </td>
               <td className="cost-all-item">
-                {store.Wallet.allSum.toFixed(2)} руб
+                {store.Wallet.allSum.toFixed(2)} {t('table.rubles')}
               </td>
             </tr>
           </tfoot>
