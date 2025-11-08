4a90d101d0e2ba7f37498b1e71f3025fb33c3c23
okoverchik <olga.koverchik@amasty.com>
2025-11-08 23:39:49 +0300

    added link on header and fixed bug redirect to wrong page

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,14 +1,20 @@
 <div class="navbar navbar-light bg-light" role="navigation">
     <div class="navbar-header header-custom-style">
-        <img src="{{ asset('images/image-for-header.png') }}" width="auto" alt="logo"/>
+        <a class="link-dark text-decoration-none" href="{{ route('main') }}" style="height: inherit">
+            <img src="{{ asset('images/image-for-header.png') }}"
+                 alt="logo"/>
+        </a>
         <h1 class="big-logo">
             <a class="link-dark text-decoration-none" aria-current="page" href="{{ route('main') }}">Bill split</a>
         </h1>
     </div>
     <ul class="nav justify-content-end">
-        <li class="nav-item">
+        <li class="nav-item d-flex align-items-center">
             @auth
-                <a class="nav-link m-4" href="{{ url('user') }}">Hi, {{ Auth::user()->name }}!</a>
+                @include('members.user-picture', ['user' => Auth::user()])
+                <a class="nav-link mr-4" href="{{ url('user') }}">
+                     {{ Auth::user()->name }}
+                </a>
             @endauth
             @guest
                 <a class="nav-link m-4" href="{{ url('login') }}">Sign in</a>
